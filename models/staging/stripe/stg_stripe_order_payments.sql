eda3c2733e4450cd2e2567bbebe5c800f8942b2d
akwashie-tm <alex.kwashie@hotmail.com>
2024-02-04 17:45:22 +0000

    commit and push our learnings into the development branch

--- models/staging/stripe/stg_stripe_order_payments.sql
+++ models/staging/stripe/stg_stripe_order_payments.sql
@@ -14,4 +14,5 @@ select
         else false
         end as is_completed_payment, 
         created as created_date
-from `dbt-tutorial.stripe.payment`
+from {{ source('stripe', 'payment')}}
+--from `dbt-tutorial.stripe.payment`
